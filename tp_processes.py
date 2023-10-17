396f5c4bac7eacfa1e5ac51809002061e7f6cae8
Michael <michaelrj03@gmail.com>
2023-10-17 21:39:22 +0200

    YES

--- tp_processes.py
+++ tp_processes.py
@@ -4,10 +4,13 @@ from copy import deepcopy
 import datetime
 import pandas as pd
 import json
+import unittest
+
 
 atm = 101300; L = 0.001; R = 8.314; k = 1.38064852e-23 # Boltzmanns konstant
 K = 10000; allowed_error = 1e-4 # number of steps and allowed error
 
+print("Loading data...")
 
 with open("data/substances.JSON","r") as file:
     substances = json.load(file)
@@ -16,10 +19,10 @@ class StaticGas:
     def __init__(self,P=None,V=None,T=None,n=None,monatomic=False,diatomic=False,name=None):
         
         if monatomic:
-            self.fluid = {"M": 4.002602, "Cv": 3/2, "Cp": 5/2, "gamma": 5/3, "formula": "He"}
+            self.fluid = {"M": 4.002602, "Cv": 3/2 * R, "Cp": 5/2 * R, "gamma": 5/3, "formula": "He"}
             self.name = "Helium"
         elif diatomic:
-            self.fluid = {"M": 28.0134, "Cv": 5/2, "Cp": 7/2, "gamma": 7/5, "formula": "N2"}
+            self.fluid = {"M": 28.0134, "Cv": 5/2 * R, "Cp": 7/2 * R, "gamma": 7/5, "formula": "N2"}
             self.name = "Nitrogen"
         # gas is a dict, so we need to check if it is in the database
         elif name in substances.keys():
@@ -38,8 +41,8 @@ class StaticGas:
         assert diatomic or monatomic or self.fluid != None
 
         self.M = self.fluid["M"]
-        self.Cv = self.fluid["Cv"]/R
-        self.Cp = self.fluid["Cp"]/R
+        self.Cv = self.fluid["Cv"]
+        self.Cp = self.fluid["Cp"]
         self.gamma = self.fluid["gamma"]
         self.formula = self.fluid["formula"]
         
@@ -77,7 +80,7 @@ Cv: {self.Cv}
 Cp: {self.Cp}
 gamma: {self.gamma}"""
 
-#print(StaticGas(P=1*atm,V=1,T=300,monatomic=True))
+print(StaticGas(P=1*atm,V=1,T=300,monatomic=True))
 
 class IdealGas(StaticGas):
     def __init__(self,n=None,P=None,V=None,T=None,monatomic=False,diatomic=False):
@@ -118,16 +121,36 @@ class IdealGas(StaticGas):
         self.ideal_gas_law_consistency = None
         self.first_law_consistency = None
 
+    
+    def define_first_law_error(self):
+        if abs(self.work+self.heat_absorbed+(self.internal_energy[-1]-self.internal_energy[0])) < allowed_error:
+            self.first_law_consistency = 1
+        if abs(self.internal_energy[-1]-self.internal_energy[0]) < allowed_error:
+            self.first_law_consistency = abs(self.work/self.heat_absorbed)
+        elif abs(self.work) < allowed_error:
+            self.first_law_consistency = abs(self.heat_absorbed/(self.internal_energy[-1]-self.internal_energy[0]))
+        elif abs(self.heat_absorbed) < allowed_error:
+            self.first_law_consistency = abs(self.work/(self.internal_energy[-1]-self.internal_energy[0]))
+        else:
+            self.first_law_consistency = abs((self.work+self.heat_absorbed)/(self.internal_energy[-1]-self.internal_energy[0]))
+    
     def _generate_extra_data(self,show):
         assert self.volume is not None and self.pressure is not None and self.temperature is not None, "Volume, pressure and temperature must be defined"
         assert self.n is not None, "Number of moles must be defined"
         assert self.Cv is not None, "Cv must be defined"
-        self.internal_energy = self.Cv*self.n*R*self.temperature
+        self.internal_energy = self.Cv*self.n*self.temperature
         self.entropy = self.n*R*np.log(self.volume)+self.Cv*np.log(self.temperature)
         self.entropy_change = self.entropy[-1]-self.entropy[0]
-        self.ideal_gas_law_consistency = self.pressure*self.volume-(self.n*R*self.temperature)
-        self.get_work()
+        self.ideal_gas_law_consistency = np.mean((self.pressure*self.volume)/(self.n*R*self.temperature))
 
+        self.get_work()
+        self.new_work()
+
+        if self.first_law_consistency != 1:
+            self.define_first_law_error()
+        else:
+            self.first_law_consistency = 1
+        
         
         if self.molar_mass != None:
             self.rms = np.sqrt(3*self.temperature*R/self.molar_mass)
@@ -135,52 +158,33 @@ class IdealGas(StaticGas):
             self.atomic_mass = self.molar_mass/6.022e23
             self.mean_free_path = 1/(np.sqrt(2)*np.pi*self.diameter**2*self.nv)
             self.mean_free_time = self.mean_free_path/self.rms
-        
-        self.first_law_consistency = self.work_done_on+self.heat_absorbed-(self.internal_energy[-1]-self.internal_energy[0])
 
-        if show: self.plot_PV()
-
-    def find_P(self,V,T):
-        return self.n*R*T/V
-    
-    def find_V(self,P,T):
-        return self.n*R*T/P
-    
-    def find_T(self,P,V):
-        return P*V/(self.n*R)
-    
-    def find_n(self,P,V,T):
-        return P*V/(R*T)
-    
-    def maxwell_boltzmann_speed_distribution(self,T=None):
-        if T == None:
-            T = np.max(self.temperature)
-        v_max = np.sqrt(2*k*T/self.atomic_mass)
-        standard_deviation = np.sqrt(k*T/self.atomic_mass)
-        v = np.linspace(v_max-3*standard_deviation,v_max+3*standard_deviation,1000)
-        return 4/np.sqrt(np.pi)*(self.atomic_mass/(k*T))**(3/2)*v**2*np.exp(-self.atomic_mass*v**2/(2*k*T))
-
-    def _find_missing(self):
-        assert (self.P != None) + (self.V != None) + (self.T != None) + (self.n != None) > 2, "Tre av P1,V1,T1 eller n må være definert"
-        if self.P == None:
-            #print("P1 er ikke definert, regner ut P1")
-            self.P = self.find_P(self.V,self.T)
-        elif self.V == None:
-            #print("V1 er ikke definert, regner ut V1")
-            self.V = self.find_V(self.P,self.T)
-        elif self.T == None:
-            #print("T1 er ikke definert, regner ut T1")
-            self.T = self.find_T(self.P,self.V)
-        elif self.n == None:
-            #print("n er ikke definert, regner ut n")
-            self.n = self.find_n(self.P,self.V,self.T)
+        # we want to check if "first law" is a nonetype
+        if isinstance(self.first_law_consistency, type(None)):
+            print("NONETYPE ERROR:", self.work, self.heat_absorbed, self.internal_energy[-1]-self.internal_energy[0])
+        elif 1.02 < self.first_law_consistency < 0.98:
+            print("VALUE ERROR:", self.work, self.heat_absorbed, self.internal_energy[-1]-self.internal_energy[0])
 
     def is_ideal_gas(self):
-        return np.all(np.abs(self.ideal_gas_law_consistency) < allowed_error)
+        result = abs(self.ideal_gas_law_consistency-1) < allowed_error
+        if result:
+            return True
+        else:
+            #print(f"Error: {self.ideal_gas_law_consistency}")
+            return False
     
     def is_first_law_satisfied(self):
-        return np.all(np.abs(self.first_law_consistency) < allowed_error)
+        result = abs(self.first_law_consistency-1) < allowed_error
+        if result:
+            return True
+        else:
+            print(f"Error: {self.internal_energy[-1]-self.internal_energy[0]} vs {self.work+self.heat_absorbed}")
+            return False
     
+    def new_work(self):
+        # we calculate the work done using numerical integration
+        self.work = np.trapz(self.pressure,self.volume)
+        self.heat = self.heat_absorbed
 
 class Isothermal(IdealGas):
     def __init__(self,n=None,T=None,V=None,P=None,monatomic=False,diatomic=False):
@@ -199,50 +203,50 @@ class Isothermal(IdealGas):
     
     def final(self,P=None,V=None,T=None,steps = K):
         if P is not None:
-            self.pressure = np.linspace(self.P,P,steps)
-            self.volume = self.find_V(self.pressure,self.T)
-            self.temperature = self.T*np.ones(steps)
+            self.pressure = np.linspace(self.pressure[0],P,steps)            
+            self.temperature = self.temperature[0]*np.ones(steps)
+            self.volume = self.n * R * self.temperature / self.pressure
         elif V is not None:
-            self.volume = np.linspace(self.V,V,steps)
-            self.pressure = self.find_P(self.volume,self.T)
-            self.temperature = self.T*np.ones(steps)
+            self.volume = np.linspace(self.volume[0],V,steps)
+            self.temperature = self.temperature[0]*np.ones(steps)
+            self.pressure = self.n * R * self.temperature / self.volume
         elif T is not None:
-            self.temperature = self.T*np.ones(steps)
-            self.volume = self.V*np.ones(steps)
-            self.pressure = self.P*np.ones(steps)
+            self.temperature = self.temperature[0]*np.ones(steps)
+            self.volume = self.volume[0]*np.ones(steps)
+            self.pressure = self.pressure[0]*np.ones(steps)
+            self.first_law_consistency = 1
         else:
             raise ValueError("P, V or T must be defined")
-        self._generate_extra_data(False)
-                        
+        self._generate_extra_data(False)                  
 class Isobaric(IdealGas):
     def __init__(self,n=None,P=None,T=None,V=None,monatomic=False,diatomic=False):
         super().__init__(n,P=P,V=V,T=T,monatomic=monatomic,diatomic=diatomic)
         self.title = "Isobar prosess"
-        self._find_missing()
     
     def get_work(self):
         self.work_done_by = self.P*(self.volume[-1]-self.volume[0])
         self.work_done_on = -self.work_done_by
-        self.heat_absorbed = self.n*R*self.Cp*(self.temperature[-1]-self.temperature[0])
+        self.heat_absorbed = self.n*self.Cp*(self.temperature[-1]-self.temperature[0])
         self.heat_released = -self.heat_absorbed
     
     def final(self,P=None,V=None,T=None,steps = K):
         if P is not None:
-            self.pressure = self.P*np.ones(steps)
-            self.volume = self.V*np.ones(steps)
-            self.temperature = self.T*np.ones(steps)
+            self.pressure = self.pressure[0]*np.ones(steps)
+            self.volume = self.volume[0]*np.ones(steps)
+            self.temperature = self.temperature[0]*np.ones(steps)
+            self.first_law_consistency = 1
+            self.ideal_gas_law_consistency = 1
         elif V is not None:
-            self.volume = np.linspace(self.V,V,steps)
-            self.pressure = self.P*np.ones(steps)
-            self.temperature = self.find_T(self.P,self.volume)
+            self.volume = np.linspace(self.volume[0],V,steps)
+            self.pressure = self.pressure[0]*np.ones(steps)
+            self.temperature = self.pressure*self.volume/(self.n*R)
         elif T is not None:
-            self.temperature = np.linspace(self.T,T,steps)
-            self.pressure = self.P*np.ones(steps)
-            self.volume = self.find_V(self.P,self.temperature)
+            self.temperature = np.linspace(self.temperature[0],T,steps)
+            self.pressure = self.pressure[0]*np.ones(steps)
+            self.volume = self.n*R*self.temperature/self.pressure
         else:
             raise ValueError("P, V or T must be defined")
         self._generate_extra_data(False)
-
 class Isochoric(IdealGas):
     def __init__(self,n=None,V=None,T=None,P=None,monatomic=False,diatomic=False):
         super().__init__(n,P=P,V=V,T=T,monatomic=monatomic,diatomic=diatomic)
@@ -251,26 +255,26 @@ class Isochoric(IdealGas):
     def get_work(self):
         self.work_done_by = 0
         self.work_done_on = 0
-        self.heat_absorbed = self.n*self.Cv*R*(self.temperature[-1]-self.temperature[0])
+        self.heat_absorbed = self.n*self.Cv*(self.temperature[-1]-self.temperature[0])
         self.heat_released = -self.heat_absorbed
     
     def final(self,P=None,V=None,T=None,steps = K):
         if P is not None:
-            self.pressure = np.linspace(self.P,P,steps)
-            self.temperature = self.find_T(self.pressure,self.V)
-            self.volume = self.V*np.ones(steps)
+            self.pressure = np.linspace(self.pressure[0],P,steps)
+            self.volume = self.volume[0]*np.ones(steps)
+            self.temperature = self.pressure * self.volume / (self.n * R)
         elif V is not None:
             self.volume = self.V*np.ones(steps)
             self.pressure = self.P*np.ones(steps)
             self.temperature = self.T*np.ones(steps)
+            self.first_law_consistency = 1
         elif T is not None:
-            self.temperature = np.linspace(self.T,T,steps)
-            self.pressure = self.find_P(self.V,self.temperature)
+            self.temperature = np.linspace(self.temperature[0],T,steps)
             self.volume = self.V*np.ones(steps)
+            self.pressure = self.n*R*self.temperature/self.volume
         else:
             raise ValueError("P, V or T must be defined")
         self._generate_extra_data(False)
-
 class Adiabatic(IdealGas):
     def __init__(self,gamma=None,n=None,P=None,V=None,T=None,monatomic=False,diatomic=False):
         super().__init__(n,P=P,V=V,T=T,monatomic=monatomic,diatomic=diatomic)
@@ -279,7 +283,7 @@ class Adiabatic(IdealGas):
         self.title = "Adiabatisk prosess"
 
     def get_work(self):
-        self.work_done_on = self.n * self.Cv * R * (self.temperature[-1]-self.T)
+        self.work_done_on = self.n * self.Cv * (self.temperature[-1]-self.T)
         self.work_done_by = -self.work_done_on
         self.heat_absorbed = 0
         self.heat_released = 0
